e46cebce53a39aefd5bbb6a7252d5d65967bd890
zouwen <zouwenotc@gmail.com>
2026-06-14 14:37:35 +0200

    Add tailwind

--- apps/game/vite.config.ts
+++ apps/game/vite.config.ts
@@ -1,7 +1,9 @@
 import type { UserConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default {
   server: {
     port: 3000,
   },
+  plugins: [tailwindcss()],
 } satisfies UserConfig;
